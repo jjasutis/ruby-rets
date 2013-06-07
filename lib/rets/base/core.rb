6c4a3cc28d92c8c3cd6d776c27dcdbe1c5dbbaf6
Jason Jasutis <jjasutis@homefinder.com>
2013-06-07 14:15:17 -0500

    initial test commit

--- lib/rets/base/core.rb
+++ lib/rets/base/core.rb
@@ -79,6 +79,7 @@ module RETS
         @request_size, @request_hash, @rets_data = nil, nil, nil
         @http.request(:url => @urls[:getmetadata], :read_timeout => args[:read_timeout], :params => {:Format => :COMPACT, :Type => args[:type], :ID => args[:id]}) do |response|
           if args[:disable_stream]
+            puts "NO STRAMING CALLING STRINGIO!!!!!!!!!!!!"
             stream = StringIO.new(response.body)
           else
             puts "STREAMING RETS:STREAMHTTP!!!!!!!!!!!!"
